c776ef4d178a5be01934be71b710bc42a2b067ae
techjanitor <puntme@gmail.com>
2015-02-20 21:37:06 -0600

    update

--- main.go
+++ main.go
@@ -8,12 +8,12 @@ import (
 )
 
 func main() {
-	k := kite.New("first", "1.0.0")
+	k := kite.New("parasail", "1.0.0")
 	k.Config.Port = 6000
 
 	k.HandleFunc("hello", Hello)
 
-	k.Register(&url.URL{Scheme: "http", Host: "localhost:6000/kite"})
+	k.Register(&url.URL{Scheme: "http", Host: "discover.modnode.com:6000/kite"})
 	k.Run()
 }
 
