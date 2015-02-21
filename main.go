8248b391c10156abb35b9f966e45e6dca564ebe0
techjanitor <puntme@gmail.com>
2015-02-20 19:59:02 -0600

    initial

--- main.go
+++ main.go
@@ -10,11 +10,18 @@ import (
 func main() {
 	k := kite.New("first", "1.0.0")
 	k.Config.Port = 6000
-	k.HandleFunc("hello", func(r *kite.Request) {
-		fmt.Println("hello!")
-		return
-	})
+
+	k.HandleFunc("hello", Hello)
 
 	k.Register(&url.URL{Scheme: "http", Host: "localhost:6000/kite"})
 	k.Run()
+}
+
+func Hello(r *kite.Request) (interface{}, error) {
+	// Print a log on remote Kite.
+	// This message will be printed on client's console.
+	r.Client.Go(fmt.Sprintf("Hello %s!", r.LocalKite.Kite().Name))
+
+	// You can return anything as result, as long as it is JSON marshalable.
+	return result, nil
 }
